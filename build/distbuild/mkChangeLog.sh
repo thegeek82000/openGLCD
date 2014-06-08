e841230b551c5431fcd2177f2d0cab4080d189bb
bperrybap <bperrybap@opensource.billsworld.billandterrie.com>
2014-06-07 21:31:37 -0600

    Added Script to automate generating changelogs

---
+++ build/distbuild/mkChangeLog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author:Andrey Nikishaev, Gunnar Lindholm
+echo "CHANGELOG"
+echo ----------------------
+git for-each-ref --sort='*authordate' --format='%(tag)' refs/tags |tac |grep -v '^$' | while read TAG ; do
+     echo
+    if [ $NEXT ];then
+        echo [$NEXT]
+    else
+        echo "[Current]"
+    fi
+    GIT_PAGER=cat git log --no-merges --format=" * %s" $TAG..$NEXT
+    NEXT=$TAG
+done
+FIRST=$(git tag -l | head -1)
+echo
+echo [$FIRST]
+GIT_PAGER=cat git log --no-merges --format=" * %s" $FIRST
